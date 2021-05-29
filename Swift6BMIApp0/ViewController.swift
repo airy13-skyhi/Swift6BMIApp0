041b49421728a1ed99b15e0f5e8618627741ed9b
github airy13-skyhi <airy13@icloud.com>
2021-05-29 13:16:22 +0900

    Initial Commit

---
+++ Swift6BMIApp0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift6BMIApp0
+//
+//  Created by Manabu Kuramochi on 2021/05/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
